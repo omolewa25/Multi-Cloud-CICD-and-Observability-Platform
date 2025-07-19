0646a0ee5665cd9674bd042576867f6d403fed7f
Omolewa Adaramola <adaramolaomolewa25@gmail.com>
2025-07-18 20:22:39 -0400

    final updates

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "alb_dns_name" {
+  value = module.compute.alb_dns_name
+}
+
+output "repo_url" {
+  value = module.ci_cd.repo_url
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
+}
